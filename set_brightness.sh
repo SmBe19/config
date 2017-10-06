21abe75f7068f4ec4488ad51d70d9d031caf1c32
Benjamin Schmid <git@schmidb.ch>
2017-10-06 10:36:12 +0200

    improve set_brightness

--- set_brightness.sh
+++ set_brightness.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+echo "current brightness is "
+cat /sys/class/backlight/intel_backlight/brightness
+echo "new brightness is "
 sudo tee /sys/class/backlight/intel_backlight/brightness <<< $1
 echo "max brightness is "
 cat /sys/class/backlight/intel_backlight/max_brightness
